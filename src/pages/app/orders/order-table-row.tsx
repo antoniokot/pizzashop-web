5eb7bd26a9e84811a0dd1a6c4189f958190816c6
Antonio Kotsubo <antoniokotsubo@gmail.com>
2024-12-10 10:28:24 -0300

    add pagination

---
+++ src/pages/app/orders/order-table-row.tsx
@@ -0,0 +1,47 @@
+import { ArrowRight, Search, X } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
+import { TableCell, TableRow } from '@/components/ui/table'
+
+// interface OrderTableRowProps {
+//   index: number
+// }
+
+export function OrderTableRow() {
+  return (
+    <TableRow>
+      <TableCell>
+        <Button variant="outline" size="xs">
+          <Search className="h-3 w-3" />
+          <span className="sr-only">Detalhes do pedido</span>
+        </Button>
+      </TableCell>
+      <TableCell className="font-mono text-sm font-medium">
+        8a608408-8665-4d2a-b4a1-16670bf5fd0b
+      </TableCell>
+      <TableCell className="text-muted-foreground">Há 15 minutos</TableCell>
+      <TableCell>
+        <div className="flex items-center gap-2">
+          <span className="h-2 w-2 rounded-full bg-slate-400" />
+          <span className="font-medium text-muted-foreground">Pendente</span>
+        </div>
+      </TableCell>
+      <TableCell className="font-medium">
+        Antônio Hideto Borges Kotsubo
+      </TableCell>
+      <TableCell className="font-medium">R$ 149,90</TableCell>
+      <TableCell>
+        <Button variant="outline" size="xs">
+          <ArrowRight className="mr-3 h-3 w-3" />
+          Aprovar
+        </Button>
+      </TableCell>
+      <TableCell>
+        <Button variant="ghost" size="xs">
+          <X className="mr-3 h-3 w-3" />
+          Cancelar
+        </Button>
+      </TableCell>
+    </TableRow>
+  )
+}
