fa81332814c19e722917ee65d083fa8d4f4baed1
Antonio Kotsubo <antoniokotsubo@gmail.com>
2025-02-17 10:06:49 -0300

    configure vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,21 @@
 import path from 'node:path'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import { configDefaults } from 'vitest/config'
+
+/// <reference types="vitest" />
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    exclude: [...configDefaults.exclude, 'node_modules'],
   },
 })
